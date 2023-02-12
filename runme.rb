a01e15dabd369d653d6b667736d1bab9d26fd58c
Brian "Burrito" Bommarito <brian@brianbommarito.xyz>
2023-02-12 11:36:19 -0500

    Using a better "happy path" schedule.

--- runme.rb
+++ runme.rb
@@ -1,15 +1,16 @@
 require_relative './lib/scheduling_service.rb'
-# meetings = [
-#   { name: "Meeting 1", duration: 3, type: :onsite },
-#   { name: "Meeting 2", duration: 2, type: :offsite },
-#   { name: "Meeting 3", duration: 1, type: :offsite },
-#   { name: "Meeting 4", duration: 0.5, type: :onsite }
-# ]
-
 meetings = [
-  { name: "Meeting 1", duration: 3, type: :offsite },
-  { name: "Meeting 3", duration: 3, type: :offsite },
-  { name: "Meeting 2", duration: 2, type: :onsite },
+  { name: "Meeting 1", duration: 1.5, type: :onsite },
+  { name: "Meeting 2", duration: 2, type: :offsite },
+  { name: "Meeting 3", duration: 1, type: :onsite },
+  { name: "Meeting 4", duration: 1, type: :offsite },
+  { name: "Meeting 5", duration: 1, type: :offsite },
 ]
+
+# meetings = [
+#   { name: "Meeting 1", duration: 3, type: :offsite },
+#   { name: "Meeting 3", duration: 3, type: :offsite },
+#   { name: "Meeting 2", duration: 2, type: :onsite },
+# ]
 
 SchedulingService.call(meetings:)
