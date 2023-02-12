561865497d5b5e82b158fb98450862b6e934bada
Brian "Burrito" Bommarito <brian@brianbommarito.xyz>
2023-02-12 09:31:19 -0500

    Initial implementation of the scheduling engine.

--- lib/scheduling_service.rb
+++ lib/scheduling_service.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class SchedulingService
   # @return [Array] the list of meetings
   attr_reader :meetings
@@ -7,12 +9,6 @@ class SchedulingService
 
   # @return [Time] the end time of the day
   attr_reader :end_time
-
-  # @return [Time] the current time of scheduling
-  attr_accessor :current_time
-
-  # @return [Array] the current schedule
-  attr_accessor :schedule
 
   # @params meetings: [Array] the list of meetings
   def self.call(meetings:)
@@ -24,15 +20,47 @@ class SchedulingService
     @meetings = meetings
     @start_time = Time.new(2021, 12, 13, 9, 0, 0) 
     @end_time = Time.new(2021, 12, 13, 17, 0, 0)
-    @current_time = @start_time
-
-    # Ah, an empty schedule. Truly, my favorite thing to see, though
-    # it's a shame it won't be empty for long.
-    @schedule = []
   end
 
   def call
-    puts "Oh, hai!"
+    # Ah, an empty schedule. Truly, my favorite thing to see, though
+    # it's a shame it won't be empty for long.
+    schedule = []
+
+    current_time = start_time
+    sorted_meetings = sort_meetings
+
+    sorted_meetings.each do |meeting|
+      # Let's first check if the meeting will fit within the
+      # alloted schedule
+
+      # We need to know if it is an offsite or onsite meeting as offsite meetings
+      # require a 30 minute padding.
+      duration = meeting[:type] == :offsite ? meeting[:duration] + 0.5 : meeting[:duration]
+
+      if current_time + (duration * 60 * 60) > end_time
+        # If it is, we'll break out and alert the user as to what meeting
+        # won't fit.
+        puts "Cannot fit #{meeting[:name]} into current schedule"
+        break
+      end
+
+      # Now, let's get these meetings scheduled, shall we? Oh, we shall.
+      if meeting[:type] == :onsite
+        start_meeting_time = current_time
+        end_meeting_time = current_time + (meeting[:duration] * 60 * 60)
+        current_time = end_meeting_time
+      elsif meeting[:type] == :offsite
+        start_meeting_time = current_time
+
+        # I pad at the end as we start the day with offsites and I assume the
+        # arrival at the offsite would be considered "beginning of day"
+        end_meeting_time = start_meeting_time + (meeting[:duration] * 60 * 60) + (0.5 * 60 * 60)
+        current_time = end_meeting_time
+      end
+
+      schedule << { name: meeting[:name], start_time: start_meeting_time, end_time: end_meeting_time }
+    end
   end
 
   # @return [Array] of sorted meetings
