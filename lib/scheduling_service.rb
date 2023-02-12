c7525095596f637abab3ffe5ed8dfa0c8d89b636
Brian "Burrito" Bommarito <brian@brianbommarito.xyz>
2023-02-12 08:07:39 -0500

    Adding a few more attr_reader and attr_accessibles.

--- lib/scheduling_service.rb
+++ lib/scheduling_service.rb
@@ -2,6 +2,18 @@ class SchedulingService
   # @return [Array] the list of meetings
   attr_reader :meetings
 
+  # @return [Time] the start time of the day
+  attr_reader :start_time
+
+  # @return [Time] the end time of the day
+  attr_reader :end_time
+
+  # @return [Time] the current time of scheduling
+  attr_accessor :current_time
+
+  # @return [Array] the current schedule
+  attr_accessor :schedule
+
   # @params meetings: [Array] the list of meetings
   def self.call(meetings:)
     self.new(meetings:).call
@@ -10,6 +22,13 @@ class SchedulingService
   # @params meetings: [Array] the list of meetings
   def initialize(meetings:)
     @meetings = meetings
+    @start_time = Time.new(2021, 12, 13, 9, 0, 0) 
+    @end_time = Time.new(2021, 12, 13, 17, 0, 0)
+    @current_time = @start_time
+
+    # Ah, an empty schedule. Truly, my favorite thing to see, though
+    # it's a shame it won't be empty for long.
+    @schedule = []
   end
 
   def call
